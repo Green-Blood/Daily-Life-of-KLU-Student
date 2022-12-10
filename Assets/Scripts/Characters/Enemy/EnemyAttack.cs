f042056fc91cf3cc25e19e18bc632af855ba7536
JeyOdilkhujaev <j.odilkhujaev@gamesredrift.com>
2022-12-10 15:47:59 +0100

    Update: Enemy attacke

--- Assets/Scripts/Characters/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Characters/Enemy/EnemyAttack.cs
@@ -6,38 +6,48 @@ using UnityEngine.AI;
 
 namespace Characters.Enemy
 {
-    public class EnemyAttack
+    public class EnemyAttack : MonoBehaviour
     {
-        private float _attackCooldown;
-        private bool _canAttack;
+        private bool _canAttack = true;
 
         private NavMeshAgent _agent;
         private EnemySettings _enemySettings;
+        private float _currentCooldown;
 
-        public EnemyAttack(EnemySettings enemySettings, NavMeshAgent enemyAgent)
+        public void Construct(EnemySettings enemySettings, NavMeshAgent enemyAgent)
         {
-            _attackCooldown = enemySettings.attackCooldown;
+            _currentCooldown = enemySettings.attackCooldown;
             _agent = enemyAgent;
-            Observable.EveryUpdate().Subscribe((l =>
-            {
-                if(!enemyAgent.isActiveAndEnabled) return;
-                if (enemyAgent.remainingDistance <= enemySettings.attackDistance)
-                {
-                    TryAttack();
-                }
-            }));
+            _enemySettings = enemySettings;
+        }
+
+        private void Update()
+        {
+            if (!_agent.isActiveAndEnabled) return;
+            CheckAttack();
+
+            if (!IsInDistance()) return;
+            if (_canAttack) TryAttack();
+        }
+
+        private void CheckAttack()
+        {
+            if (_currentCooldown > 0) _currentCooldown -= Time.deltaTime;
+            else _canAttack = true;
         }
 
         public void TryAttack()
         {
-            Observable.Timer(TimeSpan.FromSeconds(_attackCooldown)).Subscribe(l =>
-            {
-                // var player = Physics2D.OverlapCircle( _agent.destination - _agent.transform.position, _enemySettings.attackRadius,
-                //     _enemySettings.pLayerMask);
-                // if (player == null) return;
-                // if (player.TryGetComponent<IDamageable>(out var damageable))
-                //     damageable.TakeDamage(_enemySettings.attackDamage);
-            });
+            Collider2D[] results = new Collider2D[2];
+            var size = Physics2D.OverlapCircleNonAlloc(_agent.destination - _agent.transform.position,
+                _enemySettings.attackRadius, results, _enemySettings.pLayerMask);
+            _canAttack = false;
+            _currentCooldown = _enemySettings.attackCooldown;
+            if (results[0] == null) return;
+            if (results[0].TryGetComponent<IDamageable>(out var damageable))
+                damageable.TakeDamage(_enemySettings.attackDamage);
         }
+
+        private bool IsInDistance() => _agent.remainingDistance <= _enemySettings.attackDistance;
     }
 }
--- Assets/Scripts/Characters/Enemy/EnemyBootstrap.cs
+++ Assets/Scripts/Characters/Enemy/EnemyBootstrap.cs
@@ -6,24 +6,24 @@ namespace Characters.Enemy
 {
     public class EnemyBootstrap : MonoBehaviour
     {
-        [SerializeField] private CharacterHealth characterHealth;
+        [SerializeField] private CharacterHealth enemyHealth;
         [SerializeField] private EnemyDeath enemyDeath;
         [SerializeField] private EnemySettings enemySettings;
         [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private EnemyAttack enemyAttack;
 
         private Transform _playerTransform;
-        private EnemyAttack _enemyAttack;
 
         public void Construct(Transform playerTransform)
         {
-            characterHealth.Construct(enemySettings, enemyDeath);
+            enemyHealth.Construct(enemySettings, enemyDeath);
 
             _playerTransform = playerTransform;
 
             agent.speed = enemySettings.characterSpeed;
             agent.updateRotation = false;
             agent.updateUpAxis = false;
-            _enemyAttack = new EnemyAttack(enemySettings, agent);
+            enemyAttack.Construct(enemySettings, agent);
         }
 
         private void Update()
